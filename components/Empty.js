133fac73c5243c9954af189aeff251eb02699500
byuul <kimbyulyi715@gmail.com>
2024-01-25 23:51:07 +0900

    3rd chapter update empty image

--- components/Empty.js
+++ components/Empty.js
@@ -3,7 +3,11 @@ import {Image, StyleSheet, Text, View} from "react-native";
 function Empty() {
   return (
     <View style={styles.block}>
-      <Image source={require("../assets/images/circle.png")} />
+      <Image
+        source={require("../assets/images/young_and_happy.png")}
+        style={styles.image}
+        resizeMode="contain"
+      />
       <Text style={styles.description}>야호! 할일이 없습니다.</Text>
     </View>
   );
@@ -14,6 +18,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    width: 240,
+    height: 179,
+    marginBottom: 16,
   },
   description: {
     fontSize: 24,
